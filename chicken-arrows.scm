c122d66d9c6416a6788928fc1b5b2aef8956d94d
Robert Coffey <rjc@robertcoffey.xyz>
2022-03-30 23:18:48 -0230

    add reduce procedure

--- chicken-arrows.scm
+++ chicken-arrows.scm
@@ -1,5 +1,14 @@
 (import procedural-macros)
 
+(define (reduce proc lst)
+  (define (aux lst acc)
+    (if (null? lst)
+        acc
+        (aux (cdr lst) (proc acc (car lst)))))
+  (if (null? lst)
+      #f
+      (aux (cdr lst) (car lst))))
+
 ;; Insert SEXP1 as the first argument of SEXP2 before evaluation.
 (define-macro (-> sexp1 sexp2)
   (let ((proc (car sexp2)))
