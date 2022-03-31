75c5489ccffa54e42ff0a1d53e81ecf98599eab2
Robert Coffey <rjc@robertcoffey.xyz>
2022-03-31 13:02:36 -0230

    add module definition

--- chicken-arrows.scm
+++ chicken-arrows.scm
@@ -1,4 +1,13 @@
-(import procedural-macros)
+(module chicken-arrows
+  (reduce
+   insert-first
+   insert-last
+   simple-inserter
+   ->
+   ->>)
+
+(import scheme
+        procedural-macros)
 
 (define (reduce proc lst)
   (define (aux lst acc)
@@ -24,8 +33,10 @@
 ;; Insert the first sexp of REST as the first argument of the second sexp of
 ;; REST, the result into the next etc, before evaluation.
 (define-macro (-> . rest)
-  (reduce (simple-inserter insert-first) rest))
+  (eval `(reduce (simple-inserter insert-first) ',rest)))
 
 ;; Like -> but inserts as the last argument instead of the first.
 (define-macro (->> . rest)
-  (reduce (simple-inserter insert-last) rest))
+  (eval `(reduce (simple-inserter insert-last) ',rest)))
+
+) ; module
